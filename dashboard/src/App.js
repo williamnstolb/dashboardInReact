a867f44ab1a0c514af7e24688c0f8cac3d0c996e
Williamns <williamnstolb@gmail.com>
2022-03-29 15:52:47 -0300

    Iniciando uma aplicaçao para o portifolio. Um dashboard

---
+++ dashboard/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+  const openSidebar = () => setSidebarOpen(true);
+  const closeSidebar = () => setSidebarOpen(false);
+
+  
+
+  return (
+    <div className="container">
+      
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
